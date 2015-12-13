ebffe10a89eb90708ad83f3ee932c9e8ad19edc2
Jamoeba <Jamoeba@live.com>
2015-12-13 12:02:49 +0800

    test for form

--- server/server.py
+++ server/server.py
@@ -17,32 +17,48 @@ def hello_world():
 def readme():
     return render_template('readme.txt',abc='helloworld')
 
-def main():
-    ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    ss.bind((HOST, PORT))
-    ss.listen(5)
+@app.route('/post', method=['POST'])
+def post():
+    # todo....
 
-    while True:
-        print 'begin accept'
-        conn, addr = ss.accept()
-        # record Client address
-        log.get_log('Client addr', str(addr))
+@app.route('/get', method=['GET'])
+def get():
+    # todo....
 
-        try:
-            conn.settimeout(50)
-            buf = conn.recv(2048)
-            back_msg = handle.handle(buf)
-            if back_msg[0] == const.ILLEGAL_REQUEST:
-                raise ILLEGAL_REQUEST_EXCEPTION
-            else:
-                conn.sendall(back_msg[1])
-        except socket.timeout:
-            print 'time out'
-        except ILLEGAL_REQUEST_EXCEPTION:
-            conn.sendall(const.ILLEGAL_REQUEST)
-        conn.close()
+@app.route('/signup', method=['POST'])
+def signup():
+    uid = request.form['uid']
+    password = request.form['password']
+    if uid != '' and password != '':
+        return "get it"
+    else: return "get nothing"
 
-    ss.close()
+# def main():
+#     ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     ss.bind((HOST, PORT))
+#     ss.listen(5)
+
+#     while True:
+#         print 'begin accept'
+#         conn, addr = ss.accept()
+#         # record Client address
+#         log.get_log('Client addr', str(addr))
+
+#         try:
+#             conn.settimeout(50)
+#             buf = conn.recv(2048)
+#             back_msg = handle.handle(buf)
+#             if back_msg[0] == const.ILLEGAL_REQUEST:
+#                 raise ILLEGAL_REQUEST_EXCEPTION
+#             else:
+#                 conn.sendall(back_msg[1])
+#         except socket.timeout:
+#             print 'time out'
+#         except ILLEGAL_REQUEST_EXCEPTION:
+#             conn.sendall(const.ILLEGAL_REQUEST)
+#         conn.close()
+
+#     ss.close()
 
 if __name__ == '__main__':
     app.run()
